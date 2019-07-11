3e5bc7ffb8c7323fbabb47ff70f380a4f7237fc4
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2019-07-11 08:56:06 -0300

    add regex to detect user agent

--- app.js
+++ app.js
@@ -1,33 +1,18 @@
-const deviceAndroid = "android";
-const deviceIphone = "iphone";
-const deviceIpod = "ipod";
-
-//Initialize our user agent string to lower case.
-const userAgent = navigator.userAgent.toLowerCase();
-
-//**************************
-// Detects if the current device is an iPhone.
 const isIphone = () => {
-  if (userAgent.search(deviceIphone) > -1) return true;
+  if (navigator.userAgent.match(/iphone/i)) return true;
   return false;
 };
 
-//**************************
-// Detects if the current device is an iPod Touch.
 const isIpod = () => {
-  if (userAgent.search(deviceIpod) > -1) return true;
+  if (navigator.userAgent.match(/ipod/i)) return true;
   return false;
 };
 
-//**************************
-// Detects if the current device is an Android.
 const isAndroid = () => {
-  if (userAgent.search(deviceAndroid) > -1) return true;
+  if (navigator.userAgent.match(/android/i)) return true;
   return false;
 };
 
-//**************************
-// Detects if the current device is a mobile device.
 const isMobile = () => {
   if (isAndroid()) return true;
   else if (isIphone()) return true;
